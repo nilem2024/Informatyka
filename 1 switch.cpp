c5e95836c68e2dc83200037804103bdd651289f6
nilem2024 <nilemyou@gmail.com>
2024-11-20 13:16:31 +0100

    Add files via upload

---
+++ 1 switch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int liczba;
+
+cout << "Podaj pierwsza liczba : ";
+cin >> liczba;
+cout << "Podaj druga liczba : ";
+cin >> liczba;
+int a = 45;
+int b = a;
+int c = b * (a - b);
+switch(liczba) {
+    default:
+        cout << "wynik" << endl;
+        break;
+}
+    return 0;
+}
