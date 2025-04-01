46cc203043c7676e1ba9359c5c50fb156eb2d5b3
nilem2024 <nilemyou@gmail.com>
2025-04-01 11:04:42 +0200

    Sprawdzenie, czy liczba jest parzysta lub nieparzysta

--- Sprawdzenie, czy liczba jest parzysta lub nieparzysta.cpp
+++ Sprawdzenie, czy liczba jest parzysta lub nieparzysta.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
+
+
 using namespace std;
 
-int Dodaj(int a, int b) {
-    return a + b;
-}
 
 int main() {
-    int liczba1, liczba2, suma;
-
-    cout << "Podaj pierwsza liczbe calkowita: ";
-    cin >> liczba1;
-    cout << "Podaj druga liczbe calkowita: ";
-    cin >> liczba2;
-
-    suma = Dodaj(liczba1, liczba2);
-    cout << "Suma: " << suma << endl;
-
-    return 0;
+	int a = 2;
+	
+	cout << " Podaj liczbe calkowita: "
+	cin >> liczba;
+	
+	bool wynika = Funkcja;
+	
+	if(wynik == true)
+		cout " Liczba to 2" ; 
+}  else {
+	 cout << " Inna liczba ";
 }
