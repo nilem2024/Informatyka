97828a01b4e835d68896ea676f1f51a197ea355c
nilem2024 <nilemyou@gmail.com>
2025-04-02 13:14:02 +0200

    dokonczyc

---
+++ Kalkulator funkcji (nie do konca).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int liczba1, liczba2, suma;
+    
+    cout << "Podaj pierwsza liczbe calkowita: ";
+    cin >> liczba1;
+    cout << "Podaj druga liczbe calkowita: ";
+    cin >> liczba2;
+    
+     suma = ( liczba1 + liczba2 );     
+     cout << "Suma: " << suma << endl;
+	return 0;
+}
