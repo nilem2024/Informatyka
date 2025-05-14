89d32bf4170f21d5c0f6ac9c96e86309c885658a
nilem2024 <nilemyou@gmail.com>
2025-05-14 15:03:46 +0200

    Rekurencja i przeciążenie funkcji
    Rekurencja i przeciążenie funkcji

---
+++ Zadania/rfwf.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int ObliczPole(int bok) {
+	return bok * bok;
+}
+
+int ObliczPole(int dlugosc, int szerokosc) {
+	return dlugosc * szerokosc;
+}
+
+double ObliczPole(double promien) {
+	return 3.14159 * promien * promien;
+}
+
+int main() {
+	cout << "Pole kwadratu (5): " << ObliczPole(5) << endl;
+	cout << "Pole kwadratu (5, 10): " << ObliczPole(5, 10) << endl;
+	cout << "Pole kwadratu (3.5): " << ObliczPole(3.5) << endl;
+	cout << "tralalelo lalala" << endl;
+	return 0;
+}
